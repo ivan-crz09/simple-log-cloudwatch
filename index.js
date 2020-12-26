4cb3b752f6480b7c753518fdf1bd6e2bcdca8b5e
Iván Cruz <ivan.crz09@gmail.com>
2020-12-26 14:01:30 -0600

    add new changes

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('first test log')
+class simpleLogCloudwatch {
+	constructor() {
+        console.log('pruebaaaaaaaa')
+    }
+	config = () => {
+		try {
+			if (!config.aws || !config.cw) {
+				throw new Error('You need to stablish the credentialsssssssss');
+			}
+			AWS.config.update(config.aws);
+			//{apiVersion: {a.cw}}
+			cloudwatchlogs = new AWS.CloudWatchLogs({
+				apiVersion: config.cw.apiVersion
+			});
+		} catch (error) {
+			throw error;
+		}
+	};
+}
+
+module.exports = {
+    simpleLogCloudwatch
+} 
